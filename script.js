0adfe9850a8aa996dcc02b9d76dc6f94685946be
Ankit Singh <as5806801@gmail.com>
2025-08-07 12:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-var tablinks =document.getElementsByClassName("tab-links");
-var tabcontains =document.getElementsByClassName("tab-contains");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontains = document.getElementsByClassName("tab-contains");
 
-function opentab(tabname){
-    for(tabl of tablinks){
+function opentab(tabname, event) {
+    for (let tabl of tablinks) {
         tabl.classList.remove("active-link");
     }
-    for(tabcont of tabcontains){
+    for (let tabcont of tabcontains) {
         tabcont.classList.remove("active-tab");
-
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
-    
-    
-}
-var sidemenu =Document.getElementById("sidemenu");
-function openmenu (){
-    sidemenu.style.right="0";
-
-}
-function closemenu (){
-    sidemenu.style.right="-200px";
-
-    
 }
 
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
